f27b853acc6d97478b621ee4a19a22702b574a9a
Syntactual <elanjoshrand@gmail.com>
2018-02-10 10:36:05 -0600

    dammit

--- app/models/event.model.js
+++ app/models/event.model.js
@@ -2,6 +2,8 @@
 export class Event extends BaseEntity{
     constructor(){
         this.name;
-        this.date;  
+        this.date; 
+        this.locationName;
+        this.adminId; 
     }
 }
