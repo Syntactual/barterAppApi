04a9e26d8bb950788954ad55187f04c2be82c7ee
Syntactual <elanjoshrand@gmail.com>
2018-02-10 15:24:10 -0600

    added enable cors

--- app/controllers/group.controller.js
+++ app/controllers/group.controller.js
@@ -1,10 +1,48 @@
+const AWS = require("aws-sdk");
+const Guid = require('guid');
+
+AWS.config.update({
+  region: "us-west-2"
+});
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const table = "barter"
+
 exports.create = (req, res) => {
   // Create and Save a new Group
   if(!req.body.content) {
     res.status(400).send({message: "Group can not be empty"});
+ 
 
+  }else{
+    let group = req.body;
+    let user = req.locals.user;
 
+    console.log(req.locals.user);
+
+    let params = {
+      TableName:table,
+      Item:{
+          "GroupID": Guid.raw(),
+          "groupName": group.name,
+          "user": {
+            "firstName" : user.firstName
+          }
+      } 
   }
+
+  console.log("Adding a new group...");
+  docClient.put(params, function(err, data) {
+    if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+    }
+});
+  res.send();
+
+};
 
   // save issue
 
--- app/server.js
+++ app/server.js
@@ -2,21 +2,29 @@
 const express = require("express"),
     CognitoExpress = require("cognito-express"),
     port = process.env.PORT;
+const bodyParser = require('body-parser')
 
 const app = express(),
     authenticatedRoute = express.Router();
 
 const cognitoExpress = new CognitoExpress({
-    region: "us-east-1",
-    cognitoUserPoolId: "us-east-1_dXlFef73t",
+    region: "us-west-2",
+    cognitoUserPoolId: "us-west-2_laH0fT8n9",
     tokenUse: "access", //Possible Values: access | id
     tokenExpiration: 3600000 //Up to default expiration of 1 hour (3600000 ms)
 });
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.use('/api/v1',function(req, res, next) {
     
     //I'm passing in the access token in header under key accessToken
     let accessTokenFromClient = req.headers.accesstoken;
+   
  
     //Fail if token not present in header. 
     if (!accessTokenFromClient) return res.status(401).send("Access Token missing from header");
@@ -31,6 +39,8 @@ app.use('/api/v1',function(req, res, next) {
         next();
     });
 });
+
+app.use(bodyParser.json());
 
 require('./routes/items.routes.js')(authenticatedRoute);
 require('./routes/groups.routes.js')(authenticatedRoute);
